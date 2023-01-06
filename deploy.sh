86c75b88de7dd8a30fec2b4594ecea828c30454f
Ashwagandhae <julianlianbauer@gmail.com>
2023-01-06 17:19:24 +0100

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Ashwagandhae/oma-recipe.git master:gh-pages
+git push -f git@github.com:Ashwagandhae/oma-recipe.git main:gh-pages
 cd -
